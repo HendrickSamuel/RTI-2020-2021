9e3f031644aaa5ff2216059544471b179d12687f
Delaval <delaval.k60@gmail.com>
2020-09-18 13:33:06 +0200

    Corrections + lib

--- serveurs/Serveur_Mouvements.cpp
+++ serveurs/Serveur_Mouvements.cpp
@@ -1,5 +1,5 @@
 /***********************************************************/
-/*Auteur : HENDRICK Samuel et DELAVAL Kevin                */
+/*Auteurs : HENDRICK Samuel et DELAVAL Kevin               */
 /*Groupe : 2302                                            */
 /*Labo : R.T.I.                                            */
 /*Date de la dernière mise à jour : 19/09/2020             */
