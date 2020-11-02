d0605a0748b6822f7ab5a325491bbb9a2deea74a
Samuel <samuel.hendrick@student.hepl.be>
2020-11-02 20:26:46 +0100

    E-commerce IOBREP

--- JAVA/Project/src/protocol/IOBREP/Day.java
+++ JAVA/Project/src/protocol/IOBREP/Day.java
@@ -1,50 +1,51 @@
 //Auteurs : HENDRICK Samuel et DELAVAL Kevin                                                
 //Groupe : 2302                                                
 //Projet : R.T.I.                                 
-//Date de la création : 24/10/2020
+//Date de la création : 02/11/2020
 
 package protocol.IOBREP;
 
-import genericRequest.DonneeRequete;
-
 import java.io.Serializable;
 
-public class DonneeEndContainerIn implements DonneeRequete, Serializable {
-    private static final long serialVersionUID = -8210794890764332648L;
+public class Day implements Serializable {
+    private static final long serialVersionUID = 3026903785198951071L;
 
     /********************************/
     /*           Variables          */
     /********************************/
-    private String idBateau;
+    private String day;
+    private int containersCharges;
+    private int containersDecharges;
 
     /********************************/
     /*         Constructeurs        */
     /********************************/
-    public DonneeEndContainerIn(String idBateau) {
-        this.idBateau = idBateau;
+    public Day(String day, int containersCharges, int containersDecharges) {
+        this.day = day;
+        this.containersCharges = containersCharges;
+        this.containersDecharges = containersDecharges;
     }
 
     /********************************/
     /*            Getters           */
     /********************************/
-    public String getIdBateau() {
-        return idBateau;
+    public String getDay() {
+        return day;
     }
 
+    public int getContainersCharges() {
+        return containersCharges;
+    }
+
+    public int getContainersDecharges() {
+        return containersDecharges;
+    }
     /********************************/
     /*            Setters           */
     /********************************/
-    public void setIdBateau(String idBateau) {
-        this.idBateau = idBateau;
-    }
 
     /********************************/
     /*            Methodes          */
     /********************************/
-
-    @Override
-    public void setFiledsFromString(String fields) {
-
-    }
 
 }
