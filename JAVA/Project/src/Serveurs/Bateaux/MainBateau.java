7bda8fcbba39ad82dcf9c5fda3cb08962f6c4fba
Samuel <samuel.hendrick@student.hepl.be>
2020-11-09 10:24:01 +0100

    New Serveur

---
+++ JAVA/Project/src/Serveurs/Bateaux/MainBateau.java
@@ -0,0 +1,37 @@
+//Auteurs : HENDRICK Samuel et DELAVAL Kevin                                                
+//Groupe : 2302                                                
+//Projet : R.T.I.                                 
+//Date de la création : 09/11/2020
+
+package Serveurs.Bateaux;
+
+import genericRequest.MyProperties;
+import lib.BeanDBAcces.BDMouvements;
+
+import java.sql.SQLException;
+
+public class MainBateau {
+
+    public static void main(String[] args)
+    {
+        MyProperties mp = new MyProperties("./Serveur_Analysis.conf");
+        int port = Integer.parseInt(mp.getContent("PORT"));
+        int nbThreads = Integer.parseInt(mp.getContent("NBTHREADS"));
+        String user = mp.getContent("BDUSER");
+        String password = mp.getContent("BDPWD");
+        String database = mp.getContent("DATABASE");
+
+
+        ServeurBateaux sb = new ServeurBateaux(port,true, nbThreads, null);
+        try
+        {
+            BDMouvements bd = new BDMouvements(user,password,database);
+            sb.set_bdMouvement(bd);
+            sb.StartServeur();
+        }
+        catch (SQLException | ClassNotFoundException throwables)
+        {
+            throwables.printStackTrace();
+        }
+    }
+}
